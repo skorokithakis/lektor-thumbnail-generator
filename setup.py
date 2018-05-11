f76c3f3f1b06a9c1a59144726a507894ffb26b22
Stavros Korokithakis <hi@stavros.io>
2018-05-11 23:38:58 +0300

    New release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lektor-thumbnail-generator',
-    version='0.3.1',
+    version='0.3.2',
     author=u'Stavros Korokithakis,,,',
     author_email='hi@stavros.io',
     url='https://github.com/skorokithakis/lektor-thumbnail-generator/',
