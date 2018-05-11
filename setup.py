fc1f6e461b00e5f515925c7b2a586c5bf530a1c6
Stavros Korokithakis <hi@stavros.io>
2018-05-11 23:29:35 +0300

    Add description

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     author=u'Stavros Korokithakis,,,',
     author_email='hi@stavros.io',
     url='https://github.com/skorokithakis/lektor-thumbnail-generator/',
+    description="This plugin automatically generates thumbnails for any images in your Lektor content. The difference between this plugin and the `thumbnail` filter is that this is geared towards content, i.e. you don't need to have any references to the images in your templates at all.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     license='MIT',
