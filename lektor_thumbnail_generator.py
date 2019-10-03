e08fdd1e8aa17dfa54a69c5f3fd4e09152169d64
Stavros Korokithakis <hi@stavros.io>
2019-10-03 16:05:06 +0300

    Produce dramatically smaller images

--- lektor_thumbnail_generator.py
+++ lektor_thumbnail_generator.py
@@ -4,16 +4,61 @@ import shutil
 from lektor.build_programs import AttachmentBuildProgram, buildprogram
 from lektor.context import get_ctx
 from lektor.db import Image
-from lektor.imagetools import compute_dimensions, get_image_info, process_image
+from lektor.imagetools import (computed_height, find_imagemagick,
+                               get_image_info, get_quality)
 from lektor.pluginsystem import Plugin
+from lektor.reporter import reporter
+from lektor.utils import portable_popen
 from werkzeug.utils import cached_property
+
+
+# We override process_image here because Lektor does not support adding extra
+# parameters yet, but it will soon, and this can be removed when it does.
+def process_image(
+    ctx,
+    source_image,
+    dst_filename,
+    width=None,
+    height=None,
+    mode=None,
+    quality=None,
+    extra_params=None,
+):
+    """Build image from source image, optionally compressing and resizing.
+    "source_image" is the absolute path of the source in the content directory,
+    "dst_filename" is the absolute path of the target in the output directory.
+    """
+    if width is None and height is None:
+        raise ValueError("Must specify at least one of width or height.")
+
+    im = find_imagemagick(ctx.build_state.config["IMAGEMAGICK_EXECUTABLE"])
+
+    if quality is None:
+        quality = get_quality(source_image)
+
+    resize_key = ""
+    if width is not None:
+        resize_key += str(width)
+    if height is not None:
+        resize_key += "x" + str(height)
+
+    cmdline = [im, source_image, "-auto-orient"]
+    cmdline += ["-resize", resize_key]
+
+    if extra_params:
+        cmdline.extend(extra_params)
+
+    cmdline += ["-quality", str(quality), dst_filename]
+
+    reporter.report_debug_info("imagemagick cmd line", cmdline)
+    portable_popen(cmdline).wait()
 
 
 @buildprogram(Image)
 class ResizedImageBuildProgram(AttachmentBuildProgram):
     def build_artifact(self, artifact):
         ctx = get_ctx()
-        plugin = ctx.env.plugins['thumbnail-generator']
+        plugin = ctx.env.plugins["thumbnail-generator"]
         config = plugin.config
 
         artifact.ensure_dir()
@@ -24,7 +69,7 @@ class ResizedImageBuildProgram(AttachmentBuildProgram):
 
         source_img = artifact.source_obj.attachment_filename
 
-        with open(source_img, 'rb') as f:
+        with open(source_img, "rb") as f:
             _, w, h = get_image_info(f)
 
         # For every section in the config, we need to generate one image.
@@ -33,11 +78,11 @@ class ResizedImageBuildProgram(AttachmentBuildProgram):
             height = int(conf.get("max_height", "0"))
 
             if not height:
-                _, height = compute_dimensions(source_img, width, w, h)
+                height = computed_height(source_img, width, w, h)
 
             df = artifact.source_obj.url_path
             ext_pos = df.rfind(".")
-            dst_filename = "%s-%s.%s" % (df[:ext_pos], item, df[ext_pos + 1:])
+            dst_filename = "%s-%s.%s" % (df[:ext_pos], item, df[ext_pos + 1 :])
 
             def closure(dst_filename, source_img, width, height, resize_image=True):
                 # We need this closure, otherwise variables get updated and this
@@ -48,7 +93,21 @@ class ResizedImageBuildProgram(AttachmentBuildProgram):
                     if not resize_image:
                         shutil.copy2(source_img, artifact.dst_filename)
                     else:
-                        process_image(ctx, source_img, artifact.dst_filename, width, height)
+                        process_image(
+                            ctx,
+                            source_img,
+                            artifact.dst_filename,
+                            width,
+                            height,
+                            quality=85,
+                            extra_params=[
+                                "-strip",
+                                "-interlace",
+                                "Plane",
+                                "-gaussian-blur",
+                                "0.05",
+                            ],
+                        )
 
             # If the image is larger than the max_width, resize it, otherwise
             # just copy it.
@@ -57,9 +116,9 @@ class ResizedImageBuildProgram(AttachmentBuildProgram):
 
 
 class ThumbnailGeneratorPlugin(Plugin):
-    name = u'thumbnail-generator'
-    description = u'Add your description here.'
-    image_exts = ['png', 'jpg', 'jpeg', 'gif']
+    name = u"thumbnail-generator"
+    description = u"A configurable way to generate thumbnails."
+    image_exts = ["png", "jpg", "jpeg", "gif"]
 
     @cached_property
     def config(self):
